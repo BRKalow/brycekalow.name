67a12d869fa9ce0dcddbce4e5fbde57d7537f3cd
Bryce Kalow <br.kalow@gmail.com>
2020-11-09 00:41:52 +0000

    more tweaks

--- components/star-icon.tsx
+++ components/star-icon.tsx
@@ -5,31 +5,41 @@ import React from 'react';
  * Such a clever technique!
  */
 export const StarIcon = () => {
+    const [hasLiked, setHasLiked] = React.useState(false);
     const [active, setActive] = React.useState(false);
     const [count, setCount] = React.useState(0);
     const [wiggle, setWiggle] = React.useState(false);
     const wiggleTimer = React.useRef<NodeJS.Timeout>();
+    const activeTimer = React.useRef<NodeJS.Timeout>();
 
     React.useEffect(() => {
-        let timeout;
+        let countTimeout;
 
         if (active) {
-            timeout = setTimeout(() => setCount(cur => cur + 1), 500);
+            countTimeout = setTimeout(() => setCount(cur => cur + 1), 500);
+            activeTimer.current = setTimeout(() => {
+                setActive(false);
+                activeTimer.current = undefined;
+            }, 1000);
         }
 
         return () => {
-            if (timeout) clearTimeout(timeout);
+            if (countTimeout) clearTimeout(countTimeout);
         }
     }, [active])
 
     return (
         <button
             onMouseEnter={() => {
+                if (active) return;
                 setWiggle(true)
                 if (wiggleTimer.current) clearTimeout(wiggleTimer.current);
                 wiggleTimer.current = setTimeout(() => setWiggle(false), 500);
             }}
-            onClick={() => { setActive(cur => !cur); }}
+            onClick={() => {
+                if (!hasLiked) setHasLiked(true);
+                if (!activeTimer.current) setActive(true);
+            }}
             className={`${active ? 'active' : ''} ${wiggle ? 'wiggle' : ''}`}
         >
             <style jsx>{`
@@ -73,6 +83,7 @@ export const StarIcon = () => {
                     color: var(--secondary-font-color);
                     margin-left: 0.5rem;
                     vertical-align: middle;
+                    font-weight: bold;
                 }
 
                 button.wiggle:not(.active) .icon {
@@ -197,7 +208,7 @@ export const StarIcon = () => {
                 }
             `}</style>
             <span className="icon">
-                {active ? <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" className="star-icon"><path fillRule="evenodd" d="M12.672.668a.75.75 0 00-1.345 0L8.27 6.865l-6.838.994a.75.75 0 00-.416 1.279l4.948 4.823-1.168 6.811a.75.75 0 001.088.791L12 18.347l6.117 3.216a.75.75 0 001.088-.79l-1.168-6.812 4.948-4.823a.75.75 0 00-.416-1.28l-6.838-.993L12.672.668z"></path></svg> : <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" className="star-icon"><path fillRule="evenodd" d="M12 .25a.75.75 0 01.673.418l3.058 6.197 6.839.994a.75.75 0 01.415 1.279l-4.948 4.823 1.168 6.811a.75.75 0 01-1.088.791L12 18.347l-6.117 3.216a.75.75 0 01-1.088-.79l1.168-6.812-4.948-4.823a.75.75 0 01.416-1.28l6.838-.993L11.328.668A.75.75 0 0112 .25zm0 2.445L9.44 7.882a.75.75 0 01-.565.41l-5.725.832 4.143 4.038a.75.75 0 01.215.664l-.978 5.702 5.121-2.692a.75.75 0 01.698 0l5.12 2.692-.977-5.702a.75.75 0 01.215-.664l4.143-4.038-5.725-.831a.75.75 0 01-.565-.41L12 2.694z"></path></svg>}
+                {hasLiked ? <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" className="star-icon"><path fillRule="evenodd" d="M12.672.668a.75.75 0 00-1.345 0L8.27 6.865l-6.838.994a.75.75 0 00-.416 1.279l4.948 4.823-1.168 6.811a.75.75 0 001.088.791L12 18.347l6.117 3.216a.75.75 0 001.088-.79l-1.168-6.812 4.948-4.823a.75.75 0 00-.416-1.28l-6.838-.993L12.672.668z"></path></svg> : <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" className="star-icon"><path fillRule="evenodd" d="M12 .25a.75.75 0 01.673.418l3.058 6.197 6.839.994a.75.75 0 01.415 1.279l-4.948 4.823 1.168 6.811a.75.75 0 01-1.088.791L12 18.347l-6.117 3.216a.75.75 0 01-1.088-.79l1.168-6.812-4.948-4.823a.75.75 0 01.416-1.28l6.838-.993L11.328.668A.75.75 0 0112 .25zm0 2.445L9.44 7.882a.75.75 0 01-.565.41l-5.725.832 4.143 4.038a.75.75 0 01.215.664l-.978 5.702 5.121-2.692a.75.75 0 01.698 0l5.12 2.692-.977-5.702a.75.75 0 01.215-.664l4.143-4.038-5.725-.831a.75.75 0 01-.565-.41L12 2.694z"></path></svg>}
             </span>
             <span className="count">
                 {count}
