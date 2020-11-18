49405bae6492c2fcb007eaeb256bfc9190401bde
Bryce Kalow <br.kalow@gmail.com>
2020-11-17 21:56:39 -0600

    feat: adds fathom analytics (#17)

---
+++ hooks/use-fathom-analytics.ts
@@ -0,0 +1,27 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import * as Fathom from 'fathom-client';
+
+function onRouteChangeComplete() {
+    Fathom.trackPageview()
+}
+
+export function useFathomAnalytics() {
+    const router = useRouter();
+
+    useEffect(() => {
+        if (process.env.NODE_ENV === 'production') {
+            Fathom.load(process.env.NEXT_PUBLIC_FATHOM_SITE_ID, {
+                includedDomains: ['brycekalow.name']
+            })
+
+            // Record a pageview when route changes
+            router.events.on('routeChangeComplete', onRouteChangeComplete)
+
+            // Unassign event listener
+            return () => {
+                router.events.off('routeChangeComplete', onRouteChangeComplete)
+            }
+        }
+    }, [])
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,29 +1,27 @@
 import React from 'react';
-import App from 'next/app';
+import { AppProps } from 'next/app';
 import Head from 'next/head';
 import StyleProvider from '../components/style-provider';
-import ThemeSwitcher from '../components/theme-switcher';
-import Shapes from '../components/shapes';
+import { useFathomAnalytics } from '../hooks/use-fathom-analytics';
 import { Header } from '../components/header';
 
 import '../styles/prism-theme.css';
 import '../styles/animations.css';
 
-class MyApp extends App {
-  render() {
-    const { Component, pageProps } = this.props;
+export default function App({ Component, pageProps }: AppProps) {
+  useFathomAnalytics();
 
-    return (
-      <>
-        <Head>
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <meta name="robots" content="index,follow" />
-          <meta name="googlebot" content="index,follow" />
-          <meta name="twitter:creator" content="@brkalow" />
-          <meta name="twitter:site" content="@brkalow" />
-        </Head>
-        <script type="text/javascript" dangerouslySetInnerHTML={{
-          __html: `(function() {
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="robots" content="index,follow" />
+        <meta name="googlebot" content="index,follow" />
+        <meta name="twitter:creator" content="@brkalow" />
+        <meta name="twitter:site" content="@brkalow" />
+      </Head>
+      <script type="text/javascript" dangerouslySetInnerHTML={{
+        __html: `(function() {
           let theme;
 
           try {
@@ -43,17 +41,14 @@ class MyApp extends App {
           window.__theme = theme || 'initial';
           document.body.className = window.__theme;
         })();` }} />
-        <StyleProvider>
-          <Header />
-          <main role="main">
-            <div className="content">
-              <Component {...pageProps} />
-            </div>
-          </main>
-        </StyleProvider>
-      </>
-    );
-  }
+      <StyleProvider>
+        <Header />
+        <main role="main">
+          <div className="content">
+            <Component {...pageProps} />
+          </div>
+        </main>
+      </StyleProvider>
+    </>
+  );
 }
-
-export default MyApp;
