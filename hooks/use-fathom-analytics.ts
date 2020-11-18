0cfdcf41998a8cbf1724de855eb2d849abf56702
Bryce Kalow <br.kalow@gmail.com>
2020-11-18 03:52:33 +0000

    expand prod check

--- hooks/use-fathom-analytics.ts
+++ hooks/use-fathom-analytics.ts
@@ -2,6 +2,10 @@ import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import Fathom from 'fathom-client';
 
+function onRouteChangeComplete() {
+    Fathom.trackPageview()
+}
+
 export function useFathomAnalytics() {
     const router = useRouter();
 
@@ -10,17 +14,14 @@ export function useFathomAnalytics() {
             Fathom.load(process.env.NEXT_PUBLIC_FATHOM_SITE_ID, {
                 includedDomains: ['brycekalow.name']
             })
-        }
 
-        function onRouteChangeComplete() {
-            Fathom.trackPageview()
-        }
-        // Record a pageview when route changes
-        router.events.on('routeChangeComplete', onRouteChangeComplete)
+            // Record a pageview when route changes
+            router.events.on('routeChangeComplete', onRouteChangeComplete)
 
-        // Unassign event listener
-        return () => {
-            router.events.off('routeChangeComplete', onRouteChangeComplete)
+            // Unassign event listener
+            return () => {
+                router.events.off('routeChangeComplete', onRouteChangeComplete)
+            }
         }
     }, [])
 }
